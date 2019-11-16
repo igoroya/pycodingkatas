75b14d0a30b2b2248d064a4b4ffcf43ba14535d8
Igor Oya <igor.oya.vallejo@desy.de>
2019-11-16 11:16:27 +0100

    Add a print primes kata

--- katas/nov2019/trivial_katas.py
+++ katas/nov2019/trivial_katas.py
@@ -15,7 +15,7 @@ def is_even(number):
         raise ValueError('function not defined for this data type')
 
 
-if __name__ == '__main__':
+def test_is_even():
     for number in range(1, 25):
         print("is number: " + str(number) + " even?")
         print(is_even(number))
@@ -24,6 +24,38 @@ if __name__ == '__main__':
     print("is number: " + str(my_number) + " even?")
     print(is_even(my_number))
 
-    my_number = 145.7
-    print("is number: " + str(my_number) + " even?")
-    print(is_even(my_number))
+    # my_number = 145.7
+    # print("is number: " + str(my_number) + " even?")
+    # print(is_even(my_number))
+
+
+'''
+Find the first N primes
+'''
+def print_primes(ammount):
+
+    found_primes = [1, 2]
+    candidate = 3
+
+    while (len(found_primes) < ammount):
+        #print(candidate)
+        is_prime = True
+        for prime in found_primes[1:]:
+            if candidate % prime == 0:
+                #print('not prime')
+                is_prime = False
+                break
+        if is_prime:
+            #print('prime')
+            found_primes.append(candidate)
+        candidate += 2
+
+    print(found_primes)
+
+
+def test_print_primes():
+    print_primes(100)
+
+
+if __name__ == '__main__':
+    test_print_primes()
